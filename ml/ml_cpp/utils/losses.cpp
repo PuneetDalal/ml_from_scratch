810f1ece6ade3375f2df32b0a2fb830c1593eedf
Puneet <dalalpuneet400@gmail.com>
2026-09-19 11:54:14 +0530

    bce done

--- ml/ml_cpp/utils/losses.cpp
+++ ml/ml_cpp/utils/losses.cpp
@@ -1,10 +1,15 @@
 #include "losses.hpp"
+double bce(const Eigen :: MatrixXd& predction, const Eigen ::MatrixXd& true_value);
 double mse(const Eigen::MatrixXd& predictions , const Eigen::MatrixXd&true_val);
+Eigen::MatrixXd mse_derivative(const Eigen::MatrixXd& prediction, const Eigen::MatrixXd& true_value);
+
 double mse(const Eigen::MatrixXd& predictions , const Eigen::MatrixXd&true_val){
     return (predictions-true_val).array().square().mean();
 }
-Eigen::MatrixXd mse_derivative(const Eigen::MatrixXd& prediction, const Eigen::MatrixXd& true_value);
-
 Eigen::MatrixXd mse_derivative(const Eigen::MatrixXd& prediction, const Eigen::MatrixXd& true_value){
     return 2*(prediction-true_value)/true_value.rows();
+}
+
+double bce(const Eigen :: MatrixXd& predction, const Eigen ::MatrixXd& true_value){
+    return -1*(true_value.array()*(Eigen::log(predction.array())) + (1-true_value.array())*(Eigen::log(1-predction.array()))).mean();
 }
